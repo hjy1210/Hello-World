91ed61c2a8bc3d36efdb49d886cb483e80cbb591
Hon-Jang Yang <hjyanghj@gmail.com>
2014-05-08 00:00:06 +0800

    add insertTab
    add Tab whthin each character in source string

--- Simple.R
+++ Simple.R
@@ -3,3 +3,15 @@ paste(str,1:2,sep="")
 #[1] "A1" "B2"
 paste(paste(str,1:2,sep=""),collapse="\t")
 #[1] "A1\tB2"
+
+appendTabExceptLast<-function(v) {
+   v[-1]<-paste("\t",v[-1],sep="")
+   v
+}
+
+insertTab<-function(s) {
+  paste(appendTabExceptLast(strsplit(s,split=NULL)[[1]]),collapse="")
+}
+insertTab(c("abcd"))
+#[1] "a\tb\tc\td"
+ 
