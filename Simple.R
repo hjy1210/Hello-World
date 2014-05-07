401b5944d9e79273427bee4d960a013d22c78d1c
Hon-Jang Yang <hjyanghj@gmail.com>
2014-05-08 00:44:04 +0800

    modify insertTab
    increase efficiency of  insertTab

--- Simple.R
+++ Simple.R
@@ -9,9 +9,16 @@ appendTabExceptLast<-function(v) {
    v
 }
 
-insertTab<-function(s) {
-  paste(appendTabExceptLast(strsplit(s,split=NULL)[[1]]),collapse="")
+# each string in strings should be of the same length
+insertTab<-function(strings) {
+  t1<-sapply(strings,strsplit,split=NULL)
+  names(t1)<-NULL
+  t2<-sapply(t1,appendTabExceptLast)
+  apply(t2,2,paste,collapse="")
 }
 insertTab(c("abcd"))
 #[1] "a\tb\tc\td"
+insertTab(c("abcd","1234"))
+#[1] "a\tb\tc\td" "1\t2\t3\t4"
+
  
